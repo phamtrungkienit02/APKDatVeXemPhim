6ea5952acc9c3d5ca2b3a39b2c13c2968a0816bc
phamtrungkienit02 <2051052067kien@ou.edu.vn>
2023-06-30 16:11:35 +0700

    sign in and sign up activity

--- AppDatVeXemPhim/app/src/main/java/com/example/appdatvexemphim/MainActivity.java
+++ AppDatVeXemPhim/app/src/main/java/com/example/appdatvexemphim/MainActivity.java
@@ -2,13 +2,36 @@ package com.example.appdatvexemphim;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button btnSignUp = (Button) findViewById(R.id.btnSignUp);
+        Button btnSignIn = (Button) findViewById(R.id.btnSignIn);
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent activitySignIp = new Intent(MainActivity.this, SignInActivity.class);
+                startActivity(activitySignIp);
+            }
+        });
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent activitySignUp = new Intent(MainActivity.this, SignUpActivity.class);
+                startActivity(activitySignUp);
+            }
+        });
     }
 }
